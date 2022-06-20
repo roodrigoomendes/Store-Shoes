a484a7a05a97ea17fca574fbff5c0d1b13a012dd
Rodrigo Mendes <roodrigoomendessilva@gmail.com>
2022-06-20 03:28:34 -0300

    corrigindo imagens

--- store-shoes/src/components/Contents.jsx
+++ store-shoes/src/components/Contents.jsx
@@ -11,14 +11,14 @@ export default function Contents() {
                 aproveite por tempo limitado.
             </p>
             <section className="flex justify-center items-center flex-wrap">
-            <Product srcimage={"./src/img/jordan-1.png"} srctext={"jordan 1"}/>
-            <Product srcimage={"./src/img/jordan-2.png"} srctext={"jordan 2"}/>
-            <Product srcimage={"./src/img/jordan-3.png"} srctext={"jordan 3"}/>
-            <Product srcimage={"./src/img/jordan-4.png"} srctext={"jordan 4"}/>
-            <Product srcimage={"./src/img/jordan-5.png"} srctext={"jordan 5"}/>
-            <Product srcimage={"./src/img/jordan-6.png"} srctext={"jordan 6"}/>
-            <Product srcimage={"./src/img/jordan-7.png"} srctext={"jordan 7"}/>
-            <Product srcimage={"./src/img/jordan-8.png"} srctext={"jordan 8"}/>
+            <Product srcimage={"../src/img/jordan-1.png"} srctext={"jordan 1"}/>
+            <Product srcimage={"../src/img/jordan-2.png"} srctext={"jordan 2"}/>
+            <Product srcimage={"../src/img/jordan-3.png"} srctext={"jordan 3"}/>
+            <Product srcimage={"../src/img/jordan-4.png"} srctext={"jordan 4"}/>
+            <Product srcimage={"../src/img/jordan-5.png"} srctext={"jordan 5"}/>
+            <Product srcimage={"../src/img/jordan-6.png"} srctext={"jordan 6"}/>
+            <Product srcimage={"../src/img/jordan-7.png"} srctext={"jordan 7"}/>
+            <Product srcimage={"../src/img/jordan-8.png"} srctext={"jordan 8"}/>
             </section>
         </section>
     )
