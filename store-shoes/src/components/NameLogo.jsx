30eba6f78de15e54e3282e3b7cc6db8166f27b8e
Rodrigo Mendes <roodrigoomendessilva@gmail.com>
2022-06-20 20:34:31 -0300

    ultimas personalização de logo

--- store-shoes/src/components/NameLogo.jsx
+++ store-shoes/src/components/NameLogo.jsx
@@ -1,7 +1,8 @@
 export default function NameLogo() {
     return (
-<div className="py-2 text-center text-3xl font-semibold">
+<div className="py-2 flex justify-center items-center text-3xl font-semibold">
     <h1>StoreShoes</h1>
+    <img className="w-20 m-2" src="https://cdn.icon-icons.com/icons2/587/PNG/512/nike-dunk_icon-icons.com_55308.png" alt="Tenis Nike" />
 </div>
     )
 }
